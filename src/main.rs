1a91808540e51fa15f180f2d16d3c23971c3ff06
shane <shane@digitalsanctum.com>
2022-03-06 13:02:48 -0800

    Initial changes from cli to actix

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,47 @@
-extern crate clap;
-extern crate serde;
-extern crate serde_derive;
-extern crate serde_json;
+use actix_web::{App, guard, HttpRequest, HttpResponse, HttpServer, web};
+use actix_web::middleware::Logger;
+use log::info;
+use serde::{Deserialize, Serialize};
 
-use clap::{AppSettings, Parser, Subcommand};
-
-fn main() {
-    let cli: Cli = Cli::parse();
-    match &cli.command {
-        Command::Add { name, r#type } => {
-            println!("type: {}, name: {}", r#type, name);
-        }
-        Command::Info { name } => todo!(),
-        Command::List { name } => todo!(),
-    }
-
-    // TODO do stuff
+#[derive(Debug, Deserialize, Serialize)]
+struct Info {
+    first_name: String,
+    last_name: String,
 }
 
-#[derive(Subcommand)]
-enum Command {
-    #[clap(alias = "a", about = "Add a component", setting(AppSettings::ArgRequiredElseHelp))]
-    Add {
-        #[clap()]
-        /// The type of component. Valid values are: alias, func, secret, or tool
-        r#type: String,
-
-        /// The name of the component
-        name: String
-    },
-    #[clap(alias = "i", about = "Get information about a component", setting(AppSettings::ArgRequiredElseHelp))]
-    Info {
-
-        name: String
-    },
-    #[clap(alias = "l", about = "List all components", setting(AppSettings::ArgRequiredElseHelp))]
-    List { name: String },
+#[derive(Debug, Serialize)]
+struct Output {
+    message: String,
 }
 
-#[derive(Parser)]
-#[clap(version, author, about, setting = AppSettings::ArgRequiredElseHelp)]
-struct Cli {
-    #[clap(subcommand)]
-    command: Command,
+async fn index() -> HttpResponse {
+    info!("Hello");
+    HttpResponse::Ok().body("Hello")
+}
+
+async fn user(info: web::Json<Info>, req: HttpRequest) -> HttpResponse {
+    info!("{:?}", &req);
+    let out = Output {
+        message: format!("Hello there {}", info.first_name)
+    };
+    HttpResponse::Ok().json(out)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "info");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        let logger = Logger::default();
+        App::new()
+            .wrap(logger)
+            .route("/", web::get().to(index))
+            .route("/user", web::post().to(user)
+                .guard(guard::Header("content-type", "application/json")))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
